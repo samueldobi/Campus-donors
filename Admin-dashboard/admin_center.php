44839cdcea8636ebd87796b9ce52f23931a44e5d
samueldob <frankdobisamuel@gmail.com>
2024-11-20 09:55:11 +0100

    fixed the admin dashboard bug

--- Admin-dashboard/admin_center.php
+++ Admin-dashboard/admin_center.php
@@ -2,41 +2,39 @@
 try{
     require_once '../includes/dbh.inc.php';
 
-    // if (!isset($pdo)) {
-    //     throw new Exception("Database connection not established");
-    // }
-
+    $pdo = new PDO('mysql:host=localhost;dbname=blood_donation_app', $dbusername, $dbpassword);
     $stmt = $pdo->prepare("SELECT name, email, bloodtype FROM recipients; ");
     $stmt->execute();
 
-    // function displayRecipients($stmt){
-    //     // Check if there are any users
-    //     if ($stmt->rowCount() > 0) {
-    //         // echo "<h2 style = 'color:#0d6efd;'>Donor List</h2>";
-    //         echo "<ul >";
+    function displayRecipients($stmt){
+        // Check if there are any users
+        if ($stmt->rowCount() > 0) {
+            // echo "<h2 style = 'color:#0d6efd;'>Reciipent  List</h2>";
+            echo "<ul >";
             
-    //         // Fetch all users and display them
-    //         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    //             echo "<li style='color: #0d6efd; background-color: #f4f4f4; padding: 10px; margin-bottom: 5px;' >";
-    //             echo "Name: " . htmlspecialchars($row['name']) . " | ";
-    //             echo "Email: " . htmlspecialchars($row['email']);
-    //             echo "Email: " . htmlspecialchars($row['bloodtype']);
-    //             echo "</li>";
-    //         }
+            // Fetch all users and display them
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                echo "<li style='color: #0d6efd; background-color: #f4f4f4; padding: 10px; margin-bottom: 5px;' >";
+                echo "Name: " . htmlspecialchars($row['name']) . " | ";
+                echo "Email: " . htmlspecialchars($row['email']);
+                echo "Email: " . htmlspecialchars($row['bloodtype']);
+                echo "</li>";
+            }
             
-    //         echo "</ul>";
-    //     } else {
-    //         echo "<p>No users found.</p>";
-    //     } 
-    // }
+            echo "</ul>";
+        } else {
+            echo "<p>No users found.</p>";
+        } 
+    }
 
     // Call the function to display donors
-    // $recipientList =
-    //  displayRecipients($stmt);
+    // $recipientList = 
+    displayRecipients($stmt);
     // echo $recipientList ;
-    echo 'hello';
+    // echo 'hello';
 }    catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage() . " (Error code: " . $e->getCode() . ")");
 }
+
     $pdo = null;
 ?>
